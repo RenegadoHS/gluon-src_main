b20d0a8c6421a2cb128ecaa5968dbdbb042d7f54
Miguel Henrique Michel Riechi Filho <miguelriechi_windows8@outlook.com>
2019-04-13 15:42:13 -0300

    Added readme and some other stuff!

---
+++ materialsystem/stdshaders/buildparallax.bat
@@ -0,0 +1,71 @@
+@echo off
+setlocal
+
+set TTEXE=..\..\devtools\bin\timeprecise.exe
+if not exist %TTEXE% goto no_ttexe
+goto no_ttexe_end
+
+:no_ttexe
+set TTEXE=time /t
+:no_ttexe_end
+
+
+echo.
+echo ~~~~~~ buildallshaders %* ~~~~~~
+%TTEXE% -cur-Q
+set tt_all_start=%ERRORLEVEL%
+set tt_all_chkpt=%tt_start%
+
+
+
+set sourcedir="shaders"
+set targetdir="..\..\..\game\hl2\shaders"
+
+set BUILD_SHADER=call buildshaders.bat
+
+set ARG_X360=-x360
+set ARG_EXTRA=
+
+
+
+REM ****************
+REM usage: buildallshaders [-pc | -x360]
+REM ****************
+set ALLSHADERS_CONFIG=pc
+if /i "%1" == "-x360" goto shcfg_x360
+goto shcfg_end
+:shcfg_x360
+           set ALLSHADERS_CONFIG=x360
+:shcfg_end
+
+
+REM ****************
+REM PC SHADERS
+REM ****************
+if /i "%ALLSHADERS_CONFIG%" == "pc" (
+  %BUILD_SHADER% stdshader_dx9_plx
+)
+
+REM ****************
+REM X360 SHADERS
+REM ****************
+if /i "%ALLSHADERS_CONFIG%" == "x360" (
+  rem %BUILD_SHADER% stdshader_dx9_plx      %ARG_X360% %ARG_EXTRA%
+)
+
+REM ****************
+REM END
+REM ****************
+:end
+
+
+
+rem echo.
+if not "%dynamic_shaders%" == "1" (
+  echo Finished full buildallshaders %*
+) else (
+  echo Finished dynamic buildallshaders %*
+)
+
+%TTEXE% -diff %tt_all_start% -cur
+echo.
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Gluon Engine
+Custom Source Engine branch based on Source Engine 2007, as of now, only available in Windows in x86
+
+## Building
+
+### Windows
+
+1. Install [Perl](http://strawberryperl.com/).
+
+2. Open Perl Command Line and enter `cpan String::CRC32`
+
+3. Install Visual Studio 2005
+
+4. Open everything.sln
+
+5. Compile
+
+6. Pretty much everything that applies to Source SDK applies here!
+
+### Other
+
+[Source 2007 Tweaked](https://gitlab.com/Juesto/SE2007_src-tweaked)
